8d72e00a767aae2d7a2efcc88fbeda230e6a0c6e
LQ <lq@lq.com>
2024-03-05 11:41:00 +0800

    feat:常规更新

--- src/main/java/com/lq/spacex/SpaceXApplication.java
+++ src/main/java/com/lq/spacex/SpaceXApplication.java
@@ -1,9 +1,11 @@
 package com.lq.spacex;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lq.spacex.mapper")
 public class SpaceXApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/lq/spacex/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.lq.spacex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testC")
+public class TestController {
+
+    @GetMapping("/test")
+    public  void test(){
+        System.out.println("1111");
+    }
+}
