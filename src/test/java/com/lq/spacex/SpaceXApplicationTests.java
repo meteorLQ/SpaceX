e78bb4e4d54872da0a098502bc5119bdd4cde340
LQ <lq@lq.com>
2024-03-05 11:30:26 +0800

    feat:初始化

---
+++ src/test/java/com/lq/spacex/SpaceXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lq.spacex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
