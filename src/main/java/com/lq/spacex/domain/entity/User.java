7f20b69631a1287a6cb2f4067ee9474fe28f7e04
LQ <lq@lq.com>
2024-03-08 18:36:11 +0800

    feat:常规更新

--- src/main/java/com/lq/spacex/domain/entity/User.java
+++ src/main/java/com/lq/spacex/domain/entity/User.java
@@ -1,25 +1,29 @@
 package com.lq.spacex.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import jakarta.validation.constraints.NotBlank;
 
+@TableName("x_user")
 public class User {
-    private  String id;
-    @NotBlank(message = "[name]名称不能为空")
-    private  String name;
+    @TableId("user_id")
+    private  String userId;
+    @NotBlank(message = "[userName]名称不能为空")
+    private  String userName;
 
-    public String getId() {
-        return id;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
