c87163f364251f1cb7fd82c17ad477b017d7e7fa
shadowLQ <46966686+meteorLQ@users.noreply.github.com>
2024-03-12 23:36:26 +0800

    feat:常规更新

--- src/main/java/com/lq/spacex/domain/entity/User.java
+++ src/main/java/com/lq/spacex/domain/entity/User.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.NotBlank;
 public class User {
     @TableId("user_id")
     private  String userId;
+
     @NotBlank(message = "[userName]名称不能为空")
     private  String userName;
 
