f91e717410858df21c0a5df92d83b7b58b7208bc
shadowLQ <46966686+meteorLQ@users.noreply.github.com>
2024-06-22 14:59:15 +0800

    feat:常规更新

--- src/main/java/com/lq/spacex/controller/TestController.java
+++ src/main/java/com/lq/spacex/controller/TestController.java
@@ -19,5 +19,6 @@ public class TestController {
         System.out.println("true = " + true);
         System.out.println("true = " + true);
         System.out.println("true = " + true);
+        System.out.println("true = " + true);
     }
 }
