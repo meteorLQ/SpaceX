a9b5040f16d19fb85ffa3608ca583322b121f370
LQ <lq@lq.com>
2024-07-03 10:48:15 +0800

    feat:常规

--- src/main/java/com/lq/spacex/controller/TestController.java
+++ src/main/java/com/lq/spacex/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.lq.spacex.controller;
 
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
 import com.lq.spacex.domain.entity.Role;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +12,10 @@ public class TestController {
 
 
     @GetMapping("/test")
-    public  void test(){
+    public void test() {
         Role role = Role.builder().roleName("测试").roleId(1l).build();
         String jsonString = JSON.toJSONString(role);
         System.out.println("jsonString = " + jsonString);
     }
+
 }
