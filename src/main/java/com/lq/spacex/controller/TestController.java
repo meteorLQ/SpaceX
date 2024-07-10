5e666fc98f0da924dcda5e9edb4ab09106684dfc
LQ <lq@lq.com>
2024-07-10 14:07:03 +0800

    feat:常规

--- src/main/java/com/lq/spacex/controller/TestController.java
+++ src/main/java/com/lq/spacex/controller/TestController.java
@@ -19,12 +19,6 @@ public class TestController {
         String jsonString = JSON.toJSONString(role);
         testService.test();
         System.out.println("jsonString = " + jsonString);
-        System.out.println("jsonString = " + jsonString);
-        System.out.println("jsonString = " + jsonString);
-        System.out.println("jsonString = " + jsonString);
-        System.out.println("jsonString = " + jsonString);
-        System.out.println("jsonString = " + jsonString);
-        System.out.println("jsonString = " + jsonString);
     }
 
     public static void main(String[] args) {
@@ -32,3 +26,4 @@ public class TestController {
         System.out.println("jsonString = " + str.length());
     }
 }
+
