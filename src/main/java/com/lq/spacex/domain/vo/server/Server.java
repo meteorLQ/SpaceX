75a7804e93bd3191f08dacc85d3bc8d9629772eb
LQ <415804410@qq.com>
2024-10-21 14:07:04 +0800

    feat:服务列表

--- src/main/java/com/lq/spacex/domain/vo/server/Server.java
+++ src/main/java/com/lq/spacex/domain/vo/server/Server.java
@@ -4,6 +4,7 @@ package com.lq.spacex.domain.vo.server;
 
 import com.lq.spacex.common.utils.Arith;
 import com.lq.spacex.common.utils.ip.IpUtils;
+import lombok.Data;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.GlobalMemory;
@@ -25,6 +26,7 @@ import oshi.hardware.CentralProcessor.TickType;
  * 
  * @author LQ
  */
+@Data
 public class Server
 {
     private static final int OSHI_WAIT_SECOND = 1000;
@@ -52,57 +54,7 @@ public class Server
     /**
      * 磁盘相关信息
      */
-    private List<SysFile> sysFiles = new LinkedList<SysFile>();
-
-    public Cpu getCpu()
-    {
-        return cpu;
-    }
-
-    public void setCpu(Cpu cpu)
-    {
-        this.cpu = cpu;
-    }
-
-    public Mem getMem()
-    {
-        return mem;
-    }
-
-    public void setMem(Mem mem)
-    {
-        this.mem = mem;
-    }
-
-    public Jvm getJvm()
-    {
-        return jvm;
-    }
-
-    public void setJvm(Jvm jvm)
-    {
-        this.jvm = jvm;
-    }
-
-    public Sys getSys()
-    {
-        return sys;
-    }
-
-    public void setSys(Sys sys)
-    {
-        this.sys = sys;
-    }
-
-    public List<SysFile> getSysFiles()
-    {
-        return sysFiles;
-    }
-
-    public void setSysFiles(List<SysFile> sysFiles)
-    {
-        this.sysFiles = sysFiles;
-    }
+    private List<SysFile> sysFiles = new LinkedList<>();
 
     public void copyTo() throws Exception
     {
